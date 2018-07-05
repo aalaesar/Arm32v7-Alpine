6949ed597589262bea4b53384193047d056f03fb
Aalaesar <aalaesar@gmail.com>
2018-07-05 13:36:35 +0200

    mission arg for sed (forgot to specify the readme. Doh !)

--- scripts/dockerhub-build.sh
+++ scripts/dockerhub-build.sh
@@ -2,8 +2,8 @@
 set -xe
 $SKIP_BUILD && exit 0
 #update the readme
-sed -Ei "s@(Latest image is version)\s+[0-9.]+@\1 $ALPINE_VERSION@"
-sed -Ei "s@(https://alpinelinux.org/posts/Alpine-)[0-9.]+(-released.html)@\1$ALPINE_VERSION\2@g"
+sed -Ei "s@(Latest image is version)\s+[0-9.]+@\1 $ALPINE_VERSION@" README.md
+sed -Ei "s@(https://alpinelinux.org/posts/Alpine-)[0-9.]+(-released.html)@\1$ALPINE_VERSION\2@g" README.md
 
 git config --global user.email $CIRCLE_USERNAME
 git config --global user.name "CircleCI"
